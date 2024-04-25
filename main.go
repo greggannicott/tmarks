0900721b9f55cf2f23dfaa4e1cc165260cdd4063
Greg Gannicott <greg@gannicott.co.uk>
2024-04-25 21:48:14 +0100

    feat: Initial CLI implementation

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/urfave/cli/v2"
+)
+
+var Version = "Development"
+
+func main() {
+	app := &cli.App{
+		Name:            "tmarks",
+		Usage:           "Bookmarks for tmux",
+		HideHelpCommand: true,
+		Version:         Version,
+		Commands: []*cli.Command{
+			{
+				Name:  "add",
+				Usage: "Add a tmux session",
+				Action: func(cCtx *cli.Context) error {
+					fmt.Println("Coming soon")
+					return nil
+				},
+			},
+		},
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
+	}
+}
